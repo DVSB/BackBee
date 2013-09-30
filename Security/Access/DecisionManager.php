4c30b9931cfb8de8355c04308fadfae6f80c5f75
Charles Rouillon <charles.rouillon@lp-digital.fr>
2013-09-30 19:43:46 +0200

    Refactoring DecisionManager

--- Security/Access/DecisionManager.php
+++ Security/Access/DecisionManager.php
@@ -2,10 +2,17 @@
 
 namespace BackBuilder\Security\Access;
 
-use Symfony\Component\Security\Core\Authorization\AccessDecisionManager;
-use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AccessDecisionManager,
+    Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
+/**
+ * Class for all access decision managers that use decision voters.
+ *
+ * @category    BackBuilder
+ * @package     BackBuilder\Security\Access
+ * @copyright   Lp digital system
+ * @author      c.rouillon <rouillon.charles@gmail.com>
+ */
 class DecisionManager extends AccessDecisionManager
 {
 
@@ -16,11 +23,20 @@ class DecisionManager extends AccessDecisionManager
     private $_application;
 
     /**
-     *
+     * Allow to try BBToken in voters if access is not granted
      * @var Boolean
      */
     private $_tryBBTokenOnDenied;
 
+    /**
+     * Class constructor
+     * @param VoterInterface[] $voters                             An array of VoterInterface instances
+     * @param string           $strategy                           The vote strategy
+     * @param Boolean          $allowIfAllAbstainDecisions         Whether to grant access if all voters abstained or not
+     * @param Boolean          $allowIfEqualGrantedDeniedDecisions Whether to grant access if result are equals
+     * @param Boolean          $tryBBTokenOnDenied                 Allow to try BBToken in voters if access is not granted
+     * @throws \InvalidArgumentException
+     */
     public function __construct(array $voters, $strategy = 'affirmative', $allowIfAllAbstainDecisions = false, $allowIfEqualGrantedDeniedDecisions = true, $tryBBTokenOnDenied = true)
     {
         parent::__construct($voters, $strategy, $allowIfAllAbstainDecisions, $allowIfEqualGrantedDeniedDecisions);
@@ -28,6 +44,11 @@ class DecisionManager extends AccessDecisionManager
         $this->_tryBBTokenOnDenied = $tryBBTokenOnDenied;
     }
 
+    /**
+     * Sets the current application
+     * @param \BackBuilder\BBApplication $application
+     * @return \BackBuilder\Security\Access\DecisionManager
+     */
     public function setApplication(\BackBuilder\BBApplication $application)
     {
         $this->_application = $application;
