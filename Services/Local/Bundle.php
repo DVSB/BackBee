601caefba0f4631f25f706318031cab7fad29609
Charles Rouillon <charles.rouillon@lp-digital.fr>
2013-10-05 11:07:51 +0200

    Adding ACL feature to Bundle RPC service

--- Services/Local/Bundle.php
+++ Services/Local/Bundle.php
@@ -1,20 +1,30 @@
 <?php
+
 namespace BackBuilder\Services\Local;
 
-class Bundle extends AbstractServiceLocal {
+class Bundle extends AbstractServiceLocal
+{
+
     /**
      * @exposed(secured=true)
      * 
      */
-    public function findAll() {
+    public function findAll()
+    {
         if (NULL === $this->bbapp)
             throw new ServicesException("None BackBuilder Application provided", ServicesException::UNDEFINED_APP);
-        
+
         $result = array();
-        foreach($this->bbapp->getBundles() as $bundle) {
-            $result[] = json_decode($bundle->serialize());
+        foreach ($this->bbapp->getBundles() as $bundle) {
+            try {
+                $this->isGranted('EDIT', $bundle);
+                $result[] = json_decode($bundle->serialize());
+            } catch (\BackBuilder\Security\Exception\ForbiddenAccessException $e) {
+                continue;
+            }
         }
-        
+
         return $result;
     }
+
 }
