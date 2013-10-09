c03f1a05b7d5203b95906ca77633fb6967809556
Charles Rouillon <charles.rouillon@lp-digital.fr>
2013-10-09 14:36:32 +0200

    Refactoring of ACache and File\Cache

--- Cache/ACache.php
+++ Cache/ACache.php
@@ -2,7 +2,8 @@
 
 namespace BackBuilder\Cache;
 
-use BackBuilder\BBApplication;
+use BackBuilder\Exception\InvalidArgumentException;
+use Psr\Log\LoggerInterface;
 
 /**
  * Abstract class for cache adapters
@@ -10,19 +11,33 @@ use BackBuilder\BBApplication;
  * @category    BackBuilder
  * @package     BackBuilder\Cache
  * @copyright   Lp digital system
- * @author      c.rouillon
+ * @author      c.rouillon <rouillon.charles@gmail.com>
  */
 abstract class ACache
 {
 
     /**
-     * Class constructor
-     * @param \BackBuilder\BBApplication $application Optionnal BackBuilder Application
+     * A logger
+     * @var Psr\Log\LoggerInterface
      */
-    abstract public function __construct(BBApplication $application);
+    protected $_logger;
 
     /**
-     * Returns the available cache for the given id if found returns false else
+     * Class constructor
+     * @param array $options An array of options allowing to construct the cache adapter
+     * @param \Psr\Log\LoggerInterface $logger An optional logger
+     * @throws \BackBuilder\Cache\Exception\CacheException Occurs if the cache adapter cannot be construct
+     * @codeCoverageIgnore
+     */
+    public function __construct(array $options = array(), LoggerInterface $logger = null)
+    {
+        if (null !== $logger) {
+            $this->setLogger($logger);
+        }
+    }
+
+    /**
+     * Returns the available cache for the given id if found returns FALSE else
      * @param string $id Cache id
      * @param boolean $bypassCheck Allow to find cache without test it before
      * @param \DateTime $expire Optionnal, the expiration time (now by default)
@@ -38,7 +53,7 @@ abstract class ACache
     abstract public function test($id);
 
     /**
-     * Save some string datas into a cache record
+     * Saves some string datas into a cache record
      * @param string $id Cache id
      * @param string $data Datas to cache
      * @param int $lifetime Optional, the specific lifetime for this record 
@@ -60,4 +75,36 @@ abstract class ACache
      * @return boolean TRUE if cache is cleared FALSE otherwise
      */
     abstract public function clear();
+
+    /**
+     * Sets the cache logger
+     * @param \Psr\Log\LoggerInterface $logger
+     * @return \BackBuilder\Cache\ACache
+     * @codeCoverageIgnore
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->_logger = $logger;
+        return $this;
+    }
+
+    /**
+     * Logs a message on provided level if a logger is defined
+     * @param string $method The log level
+     * @param string $message The message to log
+     * @param array $context The logging context
+     * @throws \BackBuilder\Exception\InvalidArgumentException Occurs if the defined logger does not implement \Psr\Log\LoggerInterface
+     * @codeCoverageIgnore
+     */
+    protected function log($level, $message, array $context = array())
+    {
+        if (null !== $this->_logger) {
+            if (false === ($this->_logger instanceof LoggerInterface)) {
+                throw new InvalidArgumentException('Logger must implements \Psr\Log\LoggerInterface');
+            }
+
+            $this->_logger->log($level, $message, $context);
+        }
+    }
+
 }
