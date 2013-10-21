fc3383a3d471a8e715dec80fbe9a560956cc1022
Charles Rouillon <charles.rouillon@lp-digital.fr>
2013-10-21 12:26:23 +0200

    Refactoring BBUserToken

--- Security/Token/BBUserToken.php
+++ Security/Token/BBUserToken.php
@@ -1,56 +1,156 @@
 <?php
+
 namespace BackBuilder\Security\Token;
 
 use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
 
-class BBUserToken extends AbstractToken {
-    private $_created;
-    private $_digest;
-    private $_nonce;
-    
+/**
+ * Base class for BackBuilder token's user
+ * 
+ * @category    BackBuilder
+ * @package     BackBuilder\Security\Token
+ * @copyright   Lp digital system
+ * @author      c.rouillon <rouillon.charles@gmail.com>
+ */
+class BBUserToken extends AbstractToken
+{
+
     /**
-     * Constructor.
+     * Creation date of the token
+     * @var \DateTime 
+     */
+    private $_created;
+
+    /**
+     * Digest to be checks associated to the token
+     * @var string
+     */
+    private $_digest;
+
+    /**
+     * User's private nonce for the token
+     * @var string
+     */
+    private $_nonce;
+
+    /**
+     * Class Constructor.
      * @param array  $roles       An array of roles
      */
-    public function __construct(array $roles = array()) {
+    public function __construct(array $roles = array())
+    {
         parent::__construct($roles);
-//        parent::setAuthenticated(count($roles) > 0);
         $this->setAuthenticated(true);
     }
 
-    public function getCreated() {
+    /**
+     * Returns the creation date of the token
+     * @codeCoverageIgnore
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
         return $this->_created;
     }
-    
-    public function getCredentials() {
+
+    /**
+     * Returns the credentials (empty for this token)
+     * @codeCoverageIgnore
+     * @return string
+     */
+    public function getCredentials()
+    {
         return '';
     }
-    
-    public function getDigest() {
+
+    /**
+     * Returns the current digest
+     * @codeCoverageIgnore
+     * @return string
+     */
+    public function getDigest()
+    {
         return $this->_digest;
     }
-    
-    public function getNonce() {
+
+    /**
+     * Returns the user's private nonce
+     * @codeCoverageIgnore
+     * @return type
+     */
+    public function getNonce()
+    {
         return $this->_nonce;
     }
-    
-    public function setCreated($created) {
+
+    /**
+     * Sets the creation date
+     * @codeCoverageIgnore
+     * @param type $created
+     * @return \BackBuilder\Security\Token\BBUserToken
+     */
+    public function setCreated($created)
+    {
         $this->_created = $created;
         return $this;
     }
-    
-    public function setDigest($digest) {
+
+    /**
+     * Sets the digest
+     * @codeCoverageIgnore
+     * @param type $digest
+     * @return \BackBuilder\Security\Token\BBUserToken
+     */
+    public function setDigest($digest)
+    {
         $this->_digest = $digest;
         return $this;
     }
-    
-    public function setNonce($nonce) {
+
+    /**
+     * Sets the user's private nonce
+     * @codeCoverageIgnore
+     * @param type $nonce
+     * @return \BackBuilder\Security\Token\BBUserToken
+     */
+    public function setNonce($nonce)
+    {
         $this->_nonce = $nonce;
         return $this;
     }
-    
-    public function setUser($user) {
+
+    /**
+     * Sets the user
+     * @codeCoverageIgnore
+     * @param type $user
+     * @return \BackBuilder\Security\Token\BBUserToken
+     */
+    public function setUser($user)
+    {
         parent::setUser($user);
         return $this;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize()
+    {
+        $serialized = unserialize(parent::serialize());
+        $serialized[] = $this->_nonce;
+
+        return serialize($serialized);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function unserialize($serialized)
+    {
+        $array = unserialize($serialized);
+        $this->_nonce = array_pop($array);
+
+        parent::unserialize(serialize($array));
+    }
+
 }
