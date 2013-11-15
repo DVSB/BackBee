393a197bc6e7a4a14ec6ca6e636a28c7a0e10c8d
Charles Rouillon <charles.rouillon@lp-digital.fr>
2013-11-15 12:13:23 +0100

    Add of the new utility class Media

---
+++ Util/Media.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BackBuilder\Util;
+
+/**
+ * Set of utility methods to deal with media files
+ * 
+ * @category    BackBuilder
+ * @package     BackBuilder\Util
+ * @copyright   Lp digital system
+ * @author      c.rouillon <rouillon.charles@gmail.com>
+ */
+class Media
+{
+
+    /**
+     * Returns the computed storage filename of an element file
+     * @param \BackBuilder\ClassContent\Element\file $content
+     * @param int $folder_size Optional, size in characters of the storing folder
+     * @return string
+     * @throws \BackBuilder\Exception\InvalidArgumentException Occurs if the provided element file is empty
+     */
+    public static function getPathFromContent(\BackBuilder\ClassContent\Element\file $content, $folder_size = 3)
+    {
+        if (null === $content->getUid() || null === $content->originalname) {
+            throw new \BackBuilder\Exception\InvalidArgumentException('Ènable to compute path, the provided element file is not yet initialized');
+        }
+
+        $folder = '';
+        $filename = $content->getUid();
+
+        if (0 < $folder_size && strlen($content->getUid()) > $folder_size) {
+            $folder = substr($content->getUid(), 0, $folder_size) . '/';
+            $filename = substr($content->getUid(), $folder_size);
+        }
+
+        $extension = File::getExtension($content->originalname, true);
+
+        return $folder . $filename . $extension;
+    }
+
+}
