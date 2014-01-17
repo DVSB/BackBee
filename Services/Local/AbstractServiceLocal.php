1ae32d7993fdb5e7b3aaaace981bfe062912c2a2
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-01-17 19:17:33 +0100

    Add function render in AbstractServiceLocal

--- Services/Local/AbstractServiceLocal.php
+++ Services/Local/AbstractServiceLocal.php
@@ -2,19 +2,19 @@
 
 /*
  * Copyright (c) 2011-2013 Lp digital system
- * 
+ *
  * This file is part of BackBuilder5.
  *
  * BackBuilder5 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * BackBuilder5 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -53,7 +53,7 @@ class AbstractServiceLocal implements IServiceLocal
      */
     public function __construct()
     {
-        
+
     }
 
     /**
@@ -126,13 +126,41 @@ class AbstractServiceLocal implements IServiceLocal
         return true;
     }
 
+    /**
+     * Render template
+     *
+     * @param string $template
+     * @param array $params
+     * @return string
+     */
+    public function render($template, $params = array())
+    {
+        $result = "";
+        if (isset($template) && is_string($template)) {
+            $this->_assignParams($params);
+            $result = $this->application->getRenderer()->partial($template);
+        }
+
+        return $result;
+    }
+
+    private function _assignParams($params)
+    {
+        if (is_array($params)) {
+            foreach ($params as $key => $param) {
+                $this->application->getRenderer()->assign($key, $param);
+            }
+        }
+    }
+
     public function __get($name)
     {
-        if ($name === 'bbapp')
+        if ($name === 'bbapp') {
             return $this->_application;
-
-        if ($name === 'application' || $name === 'em')
+        }
+        if ($name === 'application' || $name === 'em') {
             return $this->{'_' . $name};
+        }
     }
 
 }
