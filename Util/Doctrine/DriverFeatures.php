d9d1be86813f21f782c487aa2500aa19a60ee38f
Charles Rouillon <charles.rouillon@lp-digital.fr>
2014-01-27 19:28:39 +0100

    Adding Util classes t Doctrine

---
+++ Util/Doctrine/DriverFeatures.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Copyright (c) 2011-2013 Lp digital system
+ * 
+ * This file is part of BackBuilder5.
+ *
+ * BackBuilder5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BackBuilder5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace BackBuilder\Util\Doctrine;
+
+use Doctrine\DBAL\Driver;
+
+/**
+ * Utility class to know supported features by the current driver
+ * 
+ * @category    BackBuilder
+ * @package     BackBuilder\Util
+ * @subpackage  Doctrine
+ * @copyright   Lp digital system
+ * @author      c.rouillon <charles.rouillon@lp-digital.fr>
+ */
+class DriverFeatures
+{
+
+    /**
+     * Drivers array supporting REPLACE command
+     * @var array
+     */
+    static private $_replace_supported_drivers = array(
+        'Doctrine\DBAL\Driver\PDOMySql\Driver',
+        'Doctrine\DBAL\Driver\Mysqli\Driver',
+        'Doctrine\DBAL\Driver\PDOSqlite\Driver'
+    );
+
+    /**
+     * Returns TRUE if the driver support REPLACE comand
+     * @param \Doctrine\DBAL\Driver $driver
+     * @return boolean
+     */
+    public static function replaceSupported(Driver $driver)
+    {
+        return in_array(get_class($driver), self::$_replace_supported_drivers);
+    }
+
+}
