7572c1af87ccf722c92cdcb11de6ae113f05f172
Harris Baptiste <harris.baptiste@lp-digital.fr>
2014-03-04 14:47:43 +0100

    fix rteedit on new content

--- Resources/js/bb5rteadapters/aloha.adapter.js
+++ Resources/js/bb5rteadapters/aloha.adapter.js
@@ -122,11 +122,7 @@ bb.RteManager.registerAdapter("aloha",{
         this.mode = "inline"; 
         this.mainNode = node;
         /*find all rte enabled contents*/
-        var self = this;
-        
-        /*load Content params via the mainnode. this should not be needed.*/
-       
-        //var editables = this.loadNodesRteParams(bb.jquery(this.mainNode).attr("data-type"));//sync call
+        var self = this;        
         var editables = node.getSubContents(); 
         var fieldPrefix = this._settings.fieldPrefix;
         if(!fieldPrefix || typeof fieldPrefix!="string") throw "aloha.adapter fieldPrefix must be a string";
@@ -134,9 +130,17 @@ bb.RteManager.registerAdapter("aloha",{
         if(bb.jquery.isArray(editables)){
             bb.jquery.each(editables, function(i,contentEl){
                 var editableNode = bb.jquery(contentEl).get(0);
-                if(Aloha.isEditable(editableNode)) return true;           
+                if(Aloha.isEditable(editableNode)) return true;    
                 var elementname = (typeof $bb(contentEl).get("element") == "string") ? $bb(contentEl).get("element") : false;
                 var rteConf = ($bb(contentEl).get("rteconf")!=-1) ? $bb(contentEl).get("rteconf") : false;
+                if(!rteConf){
+                    var elementConf = self.loadNodesRteParams(node.get("type"));
+                    if(bb.jquery.isPlainObject(elementConf)){
+                        rteConf = elementConf[elementname];
+                        var confLabel = ( rteConf ) ? rteConf: "disabled";
+                        $(contentEl).attr("data-rteconf",confLabel);
+                    }   
+                }
                 if( !elementname || !rteConf ) return true;
                 var editableConf = (("customconf" in self.rteConfig) && self.rteConfig.customconf) ? self.rteConfig.customconf[rteConf]: false;
                 if(!bb.jquery.isPlainObject(editableConf)) return true;
