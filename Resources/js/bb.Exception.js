57bb834678688194d984f100b79b26844c1a8d61
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-03-14 16:46:24 +0100

    Add comment on bb.Exception

--- Resources/js/bb.Exception.js
+++ Resources/js/bb.Exception.js
@@ -6,6 +6,11 @@
     define(["jscore"], function () {
         return (function () {
             var Exception = new JS.Class({
+                /**
+                 * BundleController contructor
+                 * 
+                 * @returns {undefined}
+                 */
                 initialize: function () {
                     this.name = '';
                     this.message = '';
@@ -13,12 +18,25 @@
                     this.stack = this.getStack();
                 },
 
+                /**
+                 * Function to build the Exception
+                 * 
+                 * @param {string} name
+                 * @param {string} message
+                 * @param {object} params
+                 * @returns {undefined}
+                 */
                 raise: function (name, message, params) {
                     this.name = name;
                     this.message = message;
                     this.params = params || {};
                 },
 
+                /**
+                 * Function to build the Exception
+                 * 
+                 * @returns {array}
+                 */
                 getStack: function() {
                     var err = new Error(),
                         stack = err.stack.split("\n"),
@@ -30,6 +48,12 @@
                     return cleanStack;
                 },
 
+                /**
+                 * Function to stock the Exception in bb.lastError ans bb.errors
+                 * 
+                 * @param {Exception} error
+                 * @returns {undefined}
+                 */
                 pushError: function (error) {
                     if (bb.errors === undefined) {
                         bb.errors = [];
@@ -38,6 +62,12 @@
                     bb.lastError = error;
                 },
 
+                /**
+                 * Function to parse a stak trace line
+                 * 
+                 * @param {string} line
+                 * @returns {object}
+                 */
                 parseStackLine : function (line) {
                     if (line === '') return;
 
