cb37ad638cd0f30c8b88905f1ae8bb80f9a4accf
Eric Chau <eriic.chau@gmail.com>
2014-03-19 10:44:30 +0000

    [Bundle] + added new protected method to BackBuilder\Bundle\BundleControllerBootstrap::throwExceptionIfNotBBUser(); + added comments and PHPDoc to its class

--- Bundle/BundleControllerBootstrap.php
+++ Bundle/BundleControllerBootstrap.php
@@ -3,7 +3,8 @@
 namespace BackBuilder\Bundle;
 
 use BackBuilder\Bundle\ABundle,
-    BackBuilder\Bundle\Exception\RequestErrorException;
+    BackBuilder\Bundle\Exception\RequestErrorException,
+    BackBuilder\FrontController\Exception\FrontControllerException;
 
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,6 +13,9 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class BundleControllerBootstrap
 {
+    /**
+     * @var BackBuilder\Bundle\ABundle
+     */
     protected $_bundle;
 
     /**
@@ -25,8 +29,12 @@ class BundleControllerBootstrap
     protected $_em;
 
     /**
-     * [__construct description]
-     * @param BBApplication $application [description]
+     * BundleControllerBootstrap's constructor; initialize 3 protected attributes:
+     *     - $_bundle
+     *     - $_application
+     *     - $_em
+     *     
+     * @param BackBuilder\Bundle\ABundle $bundle 
      */
     public function __construct(ABundle $bundle)
     {
@@ -36,10 +44,12 @@ class BundleControllerBootstrap
     }
 
     /**
-     * [__call description]
-     * @param  [type] $method [description]
-     * @param  [type] $args   [description]
-     * @return [type]         [description]
+     * It will act as the main controller of current controller; it will dispatch
+     * resquest to the right protected action method
+     * 
+     * @param  string $method 
+     * @param  mixed  $args   
+     * @throws BackBuilder\FrontController\Exception\FrontControllerException if called action is not available
      */
     public function __call($method, $args)
     {
@@ -72,22 +82,39 @@ class BundleControllerBootstrap
             $this->sendResponse($content, $statusCode, $headers);
         } else {
             throw new FrontControllerException(
-                sprintf('Unable to handle URL `%s`', $this->request->getPathInfo()), 
+                sprintf('Unable to handle URL `%s`', $this->_application->getRequest()->getPathInfo()), 
                 FrontControllerException::BAD_REQUEST
             );
         }
     }
 
     /**
-     * [sendResponse description]
-     * @param  [type]  $content    
-     * @param  integer $statusCode 
+     * Throws exception and stop current action if current user is not a bbuser
+     * 
+     * @throws BackBuilder\Bundle\Exception\RequestErrorException if current user is not a bbuser
      */
-    private function sendResponse($content, $statusCode = 200, array $contentType = array())
+    protected function throwExceptionIfNotBBUser()
+    {
+        if (null === $this->_application->getBBUserToken()) {
+            throw new RequestErrorException(
+                '[401]: Unauthorized action, you must be authenticated as BBUser!',
+                401
+            );
+        }
+    }
+
+    /**
+     * Generic way to send response back to client browser
+     * 
+     * @param  string  $content    the string to print on client browser screen
+     * @param  integer $statusCode the request status code (default: 200)
+     * @param  array   $headers    headers of the response (default: void array)
+     */
+    private function sendResponse($content, $statusCode = 200, array $headers = array())
     {
         $response = new Response();
 
-        foreach ($contentType as $name => $value) {
+        foreach ($headers as $name => $value) {
             $response->headers->set($name, $value);            
         }
 
