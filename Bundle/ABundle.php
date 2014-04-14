02ffa4f9b63343fdb114ad11d1fef19e24eb8f03
Micael Malta <m.malta@01net.fr>
2014-04-14 17:47:15 +0200

    Fix RessourceDir With unitest

--- Bundle/ABundle.php
+++ Bundle/ABundle.php
@@ -109,8 +109,10 @@ abstract class ABundle implements IObjectIdentifiable, \Serializable
             if (false === file_exists(dirname($filename))) {
                 mkdir(dirname($filename), 0755, true);
             }
-
-            copy($srcConfigFilename, $filename);
+            if(file_exists($srcConfigFilename))
+            {
+                copy($srcConfigFilename, $filename);
+            }
         } else {
             $srcFileStat = stat($srcConfigFilename);
             $dataFileStat = stat($filename);
@@ -263,12 +265,17 @@ abstract class ABundle implements IObjectIdentifiable, \Serializable
      */
     public function getResourcesDir()
     {
-        $dir = $this->getBaseDir() . DIRECTORY_SEPARATOR . 'Resources';
-        if (!file_exists($dir)) {
-            $dir = $this->getBaseDir() . DIRECTORY_SEPARATOR . 'Ressources';
+        //return $this->getBaseDir() . DIRECTORY_SEPARATOR . 'Ressources';
+
+        $resources_dir = $this->getBaseDir() . DIRECTORY_SEPARATOR . 'Ressources';
+        if ('test' === $this->_application->getContext()) {
+            $test_dir = $this->getBaseDir() . DIRECTORY_SEPARATOR . 'Test' . DIRECTORY_SEPARATOR . 'Ressources';
+            if (true === file_exists($test_dir)) {
+                $resources_dir = $test_dir;
+            }
         }
 
-        return $dir;
+        return $resources_dir;
     }
 
     /**
