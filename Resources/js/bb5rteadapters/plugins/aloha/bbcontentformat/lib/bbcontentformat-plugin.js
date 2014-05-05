41966d7d67ed9d6b0870b0f15f7f1fa54415b4a5
Harris <harris.baptiste@gmail.com>
2014-05-05 16:44:11 +0200

    fix htmlClean

--- Resources/js/bb5rteadapters/plugins/aloha/bbcontentformat/lib/bbcontentformat-plugin.js
+++ Resources/js/bb5rteadapters/plugins/aloha/bbcontentformat/lib/bbcontentformat-plugin.js
@@ -7,11 +7,10 @@
  * Exposes a new contentHandler
  * 
  **/
-
+var jQuery = bb.jquery; 
 var htmlClean = bb.baseurl+bb.resourcesdir+'js/libs/jquery.htmlclean/jquery.htmlClean.min.js';  
 define("bbcontentformat/bb5contenthandler", ['aloha','jquery',htmlClean,'aloha/contenthandlermanager'],
     function(Aloha, $, htmlClean, ContentHandlerManager){
-          
         var Bb5contentPasteHandler = ContentHandlerManager.createHandler({
             enabled : false,
             handleContent: function(content){
@@ -47,55 +46,3 @@ define([
         });
           
     });
-    
-    
-
-
-/*
-function pasteHtmlAtCaret(html, selectPastedContent) {
-    var sel, range;
-    if (window.getSelection) {
-        // IE9 and non-IE
-        sel = window.getSelection();
-        if (sel.getRangeAt && sel.rangeCount) {
-            range = sel.getRangeAt(0);
-            range.deleteContents();
-
-            // Range.createContextualFragment() would be useful here but is
-            // only relatively recently standardized and is not supported in
-            // some browsers (IE9, for one)
-            var el = document.createElement("div");
-            el.innerHTML = html;
-            var frag = document.createDocumentFragment(), node, lastNode;
-            while ( (node = el.firstChild) ) {
-                lastNode = frag.appendChild(node);
-            }
-            var firstNode = frag.firstChild;
-            range.insertNode(frag);
-
-            // Preserve the selection
-            if (lastNode) {
-                range = range.cloneRange();
-                range.setStartAfter(lastNode);
-                if (selectPastedContent) {
-                    range.setStartBefore(firstNode);
-                } else {
-                    range.collapse(true);
-                }
-                sel.removeAllRanges();
-                sel.addRange(range);
-            }
-        }
-    } else if ( (sel = document.selection) && sel.type != "Control") {
-        // IE < 9
-        var originalRange = sel.createRange();
-        originalRange.collapse(true);
-        sel.createRange().pasteHTML(html);
-        if (selectPastedContent) {
-            range = sel.createRange();
-            range.setEndPoint("StartToStart", originalRange);
-            range.select();
-        }
-    }
-}
-*/
