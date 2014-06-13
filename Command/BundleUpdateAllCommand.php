b8a06aac9578363c9109e505e29203772632a267
Ken Golovin <kenneth.golovin@lp-digital.fr>
2014-06-13 10:25:50 +0200

    [COMMAND] update annotations

--- Command/BundleUpdateAllCommand.php
+++ Command/BundleUpdateAllCommand.php
@@ -31,7 +31,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\ORM\Tools\SchemaTool;
 
 /**
- * Clears cache
+ * Update all bundles command
  *
  * @category    BackBuilder
  * @package     BackBuilder\Command
