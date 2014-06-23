3bfda26f540aa87eae275f27142c7d52bfda525d
Charles Rouillon <charles.rouillon@lp-digital.fr>
2014-06-23 18:41:53 +0200

    [NestedNode] Setting asynchronus parameter to true

--- NestedNode/Repository/NestedNodeRepository.php
+++ NestedNode/Repository/NestedNodeRepository.php
@@ -38,7 +38,7 @@ class NestedNodeRepository extends EntityRepository
 {
     public static $config = array(
         // calculate nested node values asynchronously via a CLI command
-        'nestedNodeCalculateAsync' => false
+        'nestedNodeCalculateAsync' => true
     );
     
     private function _hasValidHierarchicalDatas($node)
@@ -465,24 +465,27 @@ class NestedNodeRepository extends EntityRepository
         }
         $newRight = $newLeft + $node->getWeight() - 1;
 
-        $job = new \BackBuilder\Job\NestedNodeMoveSiblingsJob();
-        $job->setEntityManager($this->_em);
-        
-        $job->args = array(
-            'nodeId' => $node->getUid(),
-            'nodeClass' => get_class($node),
-            'previous_left' => $node->getLeftnode(),
-            'new_left' => $newLeft,
-            'delta' => $node->getWeight()
-        );
+        if (self::$config['nestedNodeCalculateAsync']) {
+            $job = new \BackBuilder\Job\NestedNodeMoveSiblingsJob();
+            $job->setEntityManager($this->_em);
 
-        $queue = new \BackBuilder\Job\Queue\RegistryQueue('NESTED_NODE');
-        $queue->setEntityManager($this->getEntityManager());
+            $job->args = array(
+                'nodeId' => $node->getUid(),
+                'nodeClass' => get_class($node),
+                'previous_left' => $node->getLeftnode(),
+                'new_left' => $newLeft,
+                'delta' => $node->getWeight()
+            );
 
-        $queue->enqueue($job);
-        
+            $queue = new \BackBuilder\Job\Queue\RegistryQueue('NESTED_NODE');
+            $queue->setEntityManager($this->getEntityManager());
+
+            $queue->enqueue($job);
+        } else {
+            $this->_detachFromTree($node)->shiftRlValues($dest, $newLeft, $node->getWeight());
+        }
+
         /* detach && room for subtree */
-        //$this->_detachFromTree($node)->shiftRlValues($dest, $newLeft, $node->getWeight());
         /* move the removed subtree back to the main tree */
         //$delta = $newLeft - 1; /* newleft - currentSubtreeleft always starts at 1) */
         //$levelDiff = $dest->getLevel() - 1; /* -1 as substree level starts with 1 */
