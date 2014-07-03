ecccab71030311aef8383aa203d3cd906f1a7631
Eric Chau <eric.chau@lp-digital.fr>
2014-07-03 18:45:19 +0200

    [Logger] + forbid date() to raise a warning in Logging\Logger.php by using '@'

--- Logging/Logger.php
+++ Logging/Logger.php
@@ -2,19 +2,19 @@
 
 /*
  * Copyright (c) 2011-2013 Lp digital system
- * 
+ *
  * This file is part of BackBuilder5.
  *
  * BackBuilder5 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * BackBuilder5 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -328,7 +328,7 @@ class Logger extends DebugStack implements LoggerInterface, SQLLogger
             return;
 
         foreach ($this->_appenders as $appender) {
-            $appender->write(array('d' => date('Y/m/d H:i:s'),
+            $appender->write(array('d' => @date('Y/m/d H:i:s'),
                 'p' => $this->_priorities[$level],
                 'm' => $message,
                 'u' => $this->_uniqid));
@@ -475,7 +475,7 @@ class Logger extends DebugStack implements LoggerInterface, SQLLogger
 
     /**
      * Get priority name by its code
-     * 
+     *
      * @param int $code
      * @return string|null
      */
