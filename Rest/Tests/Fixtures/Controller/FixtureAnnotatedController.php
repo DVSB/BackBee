38fe902719876246a0d9169a30240436897a008c
Ken Golovin <kenneth.golovin@lp-digital.fr>
2014-07-03 15:14:37 +0200

    [REST] correct annotation

--- Rest/Tests/Fixtures/Controller/FixtureAnnotatedController.php
+++ Rest/Tests/Fixtures/Controller/FixtureAnnotatedController.php
@@ -1,18 +1,38 @@
 <?php
 
 /*
- * This file is part of the FOSRestBundle package.
+ * Copyright (c) 2011-2013 Lp digital system
+ * 
+ * This file is part of BackBuilder5.
  *
- * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * BackBuilder5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BackBuilder5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace BackBuilder\Rest\Tests\Fixtures\Controller;
 
 use BackBuilder\Rest\Controller\Annotations\Pagination;
 
+/**
+ * Fixture Controller
+ *
+ * @Annotation
+ * 
+ * @category    BackBuilder
+ * @package     BackBuilder\Rest
+ * @copyright   Lp digital system
+ * @author      k.golovin
+ */
 class FixtureAnnotatedController 
 {
     /**
