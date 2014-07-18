52beb66d442302c321941e6a3dae304b9c2ad0dc
Nicolas Dufreche <nicolas.dufreche@lp-digital.fr>
2014-07-18 17:48:39 +0200

    [test] add revision classcontent test case

--- ClassContent/Tests/ClassContentTest.php
+++ ClassContent/Tests/ClassContentTest.php
@@ -23,6 +23,7 @@ namespace BackBuilder\ClassContent\Tests;
 
 use BackBuilder\ClassContent\Tests\Mock\MockContent;
 use BackBuilder\ClassContent\Element\image;
+use BackBuilder\ClassContent\Revision;
 
 /**
  * @category    BackBuilder
@@ -151,14 +152,14 @@ class ClassContentTest extends \PHPUnit_Framework_TestCase
             'title',
             '\BackBuilder\ClassContent\Element\date',
             array (
-                'default' => array ('value' => 'No title here')
+                'default' => array ('value' => 'Foo Bar Baz')
             )
         );
         $this->content->defineData(
             'title',
             '\BackBuilder\ClassContent\Element\image',
             array (
-                'default' => array ('value' => 'Drop picture here')
+                'default' => array ('value' => 'Foo Bar Baz')
             ),
             false
         );
@@ -168,15 +169,25 @@ class ClassContentTest extends \PHPUnit_Framework_TestCase
             array (
                 'default' => array ('value' => 'A date')
             ),
-            false
+            true
         );
 
-        $this->assertNotEquals('No title here', $this->content->title->value);
+        $this->assertNotEquals('Foo Bar Baz', $this->content->title->value);
+        $this->assertInstanceOf('BackBuilder\ClassContent\Element\date', $this->content->date);
 
         $this->assertTrue($this->content->isAccepted($this->content->date, 'title'));
         $this->assertTrue($this->content->isAccepted($this->content->title, 'title'));
         $this->assertFalse($this->content->isAccepted(new image(), 'title'));
 
-        $this->assertInstanceOf('BackBuilder\ClassContent\Element\date', $this->content->date);
+        $this->assertEquals('A date', $this->content->date->value);
+    }
+
+    public function testRevision()
+    {
+        $revision = new Revision();
+        $revision->setContent($this->content);
+        $this->content->setDraft($revision);
+
+        $this->assertInstanceOf('BackBuilder\ClassContent\Element\text', $this->content->getDraft()->getData('title'));
     }
 }
