b6e400302524fc716fca31e6c7e7ecf88064a77b
Ken Golovin <kenneth.golovin@lp-digital.fr>
2014-07-24 19:17:41 +0200

    [SECURITY] ACL loader

---
+++ Security/Acl/Loader/YmlLoader.php
@@ -0,0 +1,384 @@
+<?php
+
+/*
+ * Copyright (c) 2011-2013 Lp digital system
+ * 
+ * This file is part of BackBuilder5.
+ *
+ * BackBuilder5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BackBuilder5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace BackBuilder\Security\Acl\Loader;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+use Symfony\Component\Yaml\Parser;
+
+use Symfony\Component\Yaml\Yaml,
+    Symfony\Component\Security\Acl\Domain\UserSecurityIdentity,
+    Symfony\Component\Security\Acl\Domain\ObjectIdentity;
+
+use BackBuilder\Security\Acl\Permission\PermissionMap;
+use BackBuilder\Security\Acl\Permission\MaskBuilder;
+
+/**
+ * Yml Loader
+ * 
+ * Loads yml acl data into the DB
+ *
+ * @category    BackBuilder
+ * @package     BackBuilder\Security
+ * @subpackage  Acl\Loader
+ * @copyright   Lp digital system
+ * @author      k.golovin
+ */
+class YmlLoader extends ContainerAware
+{
+    protected $em;
+    protected $bbapp;
+
+    
+    public function load($ymlFile)
+    {
+        if(!file_exists($ymlFile) || !is_readable($ymlFile)) {
+            throw new \InvalidArgumentException('File cannot be read: ' . $ymlFile);
+        }
+        $aclProvider = $this->container->get('bbapp')->getSecurityContext()->getACLProvider();
+        
+        $this->em = $this->container->get('bbapp')->getEntityManager();
+        $this->bbapp = $this->container->get('bbapp');
+        
+        $grid = Yaml::parse(file_get_contents($ymlFile), true);
+        
+        if (false === array_key_exists('groups', $grid) || false === is_array($grid['groups'])) {
+            throw new \Exception('Invalid yml: ' . $ymlFile);
+        }
+        
+        foreach ($grid['groups'] as $group_identifier => $rights) {
+
+            if (null === $group = $this->em->getRepository('BackBuilder\Security\Group')->findOneBy(array('_identifier' => $group_identifier))) {
+                // ensure group exists
+                $group = new \BackBuilder\Security\Group();
+                $group->setIdentifier($group_identifier)
+                        ->setName($group_identifier);
+                $this->em->persist($group);
+                $this->em->flush($group);
+            }
+
+            $securityIdentity = new UserSecurityIdentity($group_identifier, 'BackBuilder\Security\Group');
+
+            if (true === array_key_exists('sites', $rights)) {
+                $sites = $this->addSiteRights($rights['sites'], $aclProvider, $securityIdentity);
+                foreach ($sites as $site) {
+                    if (true === array_key_exists('layouts', $rights)) {
+                        $this->addLayoutRights($rights['layouts'], $sites, $aclProvider, $securityIdentity);
+                    }
+                }
+
+                if (true === array_key_exists('pages', $rights)) {
+                    $this->addPageRights($rights['pages'], $aclProvider, $securityIdentity);
+                }
+
+                if (true === array_key_exists('mediafolders', $rights)) {
+                    $this->addFolderRights($rights['mediafolders'], $aclProvider, $securityIdentity);
+                }
+
+                if (true === array_key_exists('contents', $rights)) {
+                    $this->addContentRights($rights['contents'], $aclProvider, $securityIdentity);
+                }
+
+                if (true === array_key_exists('bundles', $rights)) {
+                    $this->addBundleRights($rights['bundles'], $aclProvider, $securityIdentity);
+                }
+            }
+        }
+    }
+    
+    
+    
+    private function addSiteRights($sites_def, $aclProvider, $securityIdentity)
+    {
+        if (false === array_key_exists('resources', $sites_def) || false === array_key_exists('actions', $sites_def)) {
+            return array();
+        }
+
+        $actions = $this->getActions($sites_def['actions']);
+        if (0 === count($actions)) {
+            return array();
+        }
+
+        $sites = array();
+        if (true === is_array($sites_def['resources'])) {
+            foreach ($sites_def['resources'] as $site_label) {
+                if (null === $site = $this->em->getRepository('BackBuilder\Site\Site')->findOneBy(array('_label' => $site_label))) {
+                    continue;
+                }
+
+                $sites[] = $site;
+                $this->addObjectAcl($site, $aclProvider, $securityIdentity, $actions);
+            }
+        } elseif ('all' === $sites_def['resources']) {
+            $sites = $this->em->getRepository('BackBuilder\Site\Site')->findAll();
+            $this->addClassAcl(new \Backbuilder\Site\Site('*'), $aclProvider, $securityIdentity, $actions);
+        }
+
+        return $sites;
+    }
+
+    private function addLayoutRights($layout_def, $sites, $aclProvider, $securityIdentity)
+    {
+        if (false === array_key_exists('resources', $layout_def) || false === array_key_exists('actions', $layout_def)) {
+            return;
+        }
+
+        $actions = $this->getActions($layout_def['actions']);
+        if (0 === count($actions)) {
+
+            return array();
+        }
+
+        foreach ($sites as $site) {
+            if (true === is_array($layout_def['resources'])) {
+                foreach ($layout_def['resources'] as $layout_label) {
+                    if (null === $layout = $this->em->getRepository('BackBuilder\Site\Layout')->findOneBy(array('_site' => $site, '_label' => $layout_label))) {
+                        continue;
+                    }
+
+                    $this->addObjectAcl($layout, $aclProvider, $securityIdentity, $actions);
+                }
+            } elseif ('all' === $layout_def['resources']) {
+                $this->addClassAcl(new \Backbuilder\Site\Layout('*'), $aclProvider, $securityIdentity, $actions);
+            }
+        }
+    }
+
+    private function addPageRights($page_def, $aclProvider, $securityIdentity)
+    {
+        if (false === array_key_exists('resources', $page_def) || false === array_key_exists('actions', $page_def)) {
+            return;
+        }
+
+        $actions = $this->getActions($page_def['actions']);
+        if (0 === count($actions)) {
+            return array();
+        }
+
+        if (true === is_array($page_def['resources'])) {
+            foreach ($page_def['resources'] as $page_url) {
+                $pages = $this->em->getRepository('BackBuilder\Site\Layout')->findBy(array('_url' => $page_url));
+                foreach ($pages as $page) {
+                    $this->addObjectAcl($page, $aclProvider, $securityIdentity, $actions);
+                }
+            }
+        } elseif ('all' === $page_def['resources']) {
+            $this->addClassAcl(new \BackBuilder\NestedNode\Page('*'), $aclProvider, $securityIdentity, $actions);
+        }
+    }
+
+    private function addFolderRights($folder_def, $aclProvider, $securityIdentity)
+    {
+        if (false === array_key_exists('resources', $folder_def) || false === array_key_exists('actions', $folder_def)) {
+            return;
+        }
+
+        $actions = $this->getActions($folder_def['actions']);
+        if (0 === count($actions)) {
+            return array();
+        }
+
+        if ('all' === $folder_def['resources']) {
+            $this->addClassAcl(new \BackBuilder\NestedNode\MediaFolder('*'), $aclProvider, $securityIdentity, $actions);
+        }
+    }
+
+    private function addContentRights($content_def, $aclProvider, $securityIdentity)
+    {
+        if (false === array_key_exists('resources', $content_def) || false === array_key_exists('actions', $content_def)) {
+            return;
+        }
+
+        $service = new \BackBuilder\Services\Local\ContentBlocks();
+        $service->initService($this->bbapp);
+        $all_classes = $service->getContentsByCategory();
+
+        if ('all' === $content_def['resources']) {
+            $actions = $this->getActions($content_def['actions']);
+            if (0 === count($actions)) {
+                return array();
+            }
+
+            $service = new \BackBuilder\Services\Local\ContentBlocks();
+            $service->initService($this->bbapp);
+            foreach ($all_classes as $content) {
+                $classname = '\BackBuilder\ClassContent\\' . $content->name;
+                $this->addClassAcl(new $classname('*'), $aclProvider, $securityIdentity, $actions);
+            }
+        } elseif (true === is_array($content_def['resources']) && 0 < count($content_def['resources'])) {
+            if (true === is_array($content_def['resources'][0])) {
+                $used_classes = array();
+                foreach($content_def['resources'] as $index => $resources_def) {
+                    if (false === isset($content_def['actions'][$index])) {
+                        continue;
+                    }
+
+                    $actions = $this->getActions($content_def['actions'][$index]);
+
+                    if ('remains' === $resources_def) {
+                        foreach($all_classes as $content) {
+                            $classname = '\BackBuilder\ClassContent\\' . $content->name;
+                            if (false === in_array($classname, $used_classes)) {
+                                $used_classes[] = $classname;
+                                if (0 < count($actions)) {
+                                    $this->addClassAcl(new $classname('*'), $aclProvider, $securityIdentity, $actions);
+                                }
+                            }
+                        }
+                    } elseif (true === is_array($resources_def)) {
+                        foreach ($resources_def as $content) {
+                            $classname = '\BackBuilder\ClassContent\\' . $content;
+                            if (substr($classname, -1) === '*') {
+                                $classname = substr($classname, 0 - 1);
+                                foreach ($all_classes as $content) {
+                                    $fullclass = '\BackBuilder\ClassContent\\' . $content->name;
+                                    if (0 === strpos($fullclass, $classname)) {
+                                        $used_classes[] = $fullclass;
+                                        if (0 < count($actions)) {
+                                            $this->addClassAcl(new $fullclass('*'), $aclProvider, $securityIdentity, $actions);
+                                        }
+                                    }
+                                }
+                            } elseif (true === class_exists($classname)) {
+                                $used_classes[] = $classname;
+                                if (0 < count($actions)) {
+                                    $this->addClassAcl(new $classname('*'), $aclProvider, $securityIdentity, $actions);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                $actions = $this->getActions($content_def['actions']);
+                if (0 === count($actions)) {
+                    return array();
+                }
+
+                foreach ($content_def['resources'] as $content) {
+                    $classname = '\BackBuilder\ClassContent\\' . $content;
+                    if (substr($classname, -1) === '*') {
+                        $classname = substr($classname, 0 -1);
+                        foreach($all_classes as $content) {
+                            $fullclass = '\BackBuilder\ClassContent\\' . $content->name;
+                            if (0 === strpos($fullclass, $classname)) {
+                                $this->addClassAcl(new $fullclass('*'), $aclProvider, $securityIdentity, $actions);
+                            }
+                        }
+                    } elseif (true === class_exists($classname)) {
+                        $this->addClassAcl(new $classname('*'), $aclProvider, $securityIdentity, $actions);
+                    }
+                }
+            }
+        }
+    }
+
+    private function addBundleRights($bundle_def, $aclProvider, $securityIdentity)
+    {
+        if (false === array_key_exists('resources', $bundle_def) || false === array_key_exists('actions', $bundle_def)) {
+            return;
+        }
+
+        $actions = $this->getActions($bundle_def['actions']);
+        if (0 === count($actions)) {
+            echo 'Notice: none actions defined on bundle' . PHP_EOL;
+            return array();
+        }
+
+        if (true === is_array($bundle_def['resources'])) {
+            foreach ($bundle_def['resources'] as $bundle_name) {
+                if (null !== $bundle = $this->bbapp->getBundle($bundle_name)) {
+                    $this->addObjectAcl($bundle, $aclProvider, $securityIdentity, $actions);
+                }
+            }
+        } elseif ('all' === $bundle_def['resources']) {
+            foreach ($this->bbapp->getBundles() as $bundle) {
+                $this->addObjectAcl($bundle, $aclProvider, $securityIdentity, $actions);
+            }
+        }
+    }
+
+    private function getActions($def)
+    {
+        $actions = array();
+        if (true === is_array($def)) {
+            $actions = array_intersect(array('view', 'create', 'edit', 'delete', 'publish'), $def);
+        } elseif ('all' === $def) {
+            $actions = array('view', 'create', 'edit', 'delete', 'publish');
+        }
+
+        return $actions;
+    }
+
+    private function addObjectAcl($object, $aclProvider, $securityIdentity, $rights)
+    {
+        $objectIdentity = ObjectIdentity::fromDomainObject($object);
+
+        try {
+            $acl = $aclProvider->findAcl($objectIdentity, array($securityIdentity));
+        } catch (\Exception $e) {
+            $acl = $aclProvider->createAcl($objectIdentity);
+        }
+
+        foreach ($rights as $right) {
+            try {
+                $map = new PermissionMap();
+                $acl->isGranted($map->getMasks(strtoupper($right), $object), array($securityIdentity));
+            } catch (\Exception $e) {
+                $builder = new MaskBuilder();
+                foreach ($rights as $right) {
+                    $builder->add($right);
+                }
+                $mask = $builder->get();
+
+                $acl->insertObjectAce($securityIdentity, $mask);
+                $aclProvider->updateAcl($acl);
+            }
+        }
+    }
+
+    private function addClassAcl($object, $aclProvider, $securityIdentity, $rights)
+    {
+        $objectIdentity = ObjectIdentity::fromDomainObject($object);
+
+        try {
+            $acl = $aclProvider->findAcl($objectIdentity, array($securityIdentity));
+        } catch (\Exception $e) {
+            $acl = $aclProvider->createAcl($objectIdentity);
+        }
+
+        foreach ($rights as $right) {
+            try {
+                $map = new PermissionMap();
+                $acl->isGranted($map->getMasks(strtoupper($right), $object), array($securityIdentity));
+            } catch (\Exception $e) {
+                $builder = new MaskBuilder();
+                foreach ($rights as $right) {
+                    $builder->add($right);
+                }
+                $mask = $builder->get();
+
+                $acl->insertClassAce($securityIdentity, $mask);
+                $aclProvider->updateAcl($acl);
+            }
+        }
+    }
+}
---
+++ Security/Tests/Acl/Loader/YmlLoaderTest.php
@@ -0,0 +1,149 @@
+<?php
+
+/*
+ * Copyright (c) 2011-2013 Lp digital system
+ * 
+ * This file is part of BackBuilder5.
+ *
+ * BackBuilder5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BackBuilder5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace BackBuilder\Security\Tests;
+
+use BackBuilder\Tests\TestCase;
+
+use BackBuilder\Security\Acl\Loader\YmlLoader;
+
+use BackBuilder\Security\User,
+    BackBuilder\Security\Group,
+    BackBuilder\Site\Site;
+
+use  BackBuilder\Security\Token\BBUserToken;
+
+/**
+ * Test for YmlLoader class
+ *
+ * @category    BackBuilder
+ * @package     BackBuilder\Security
+ * @subpackage  Tests
+ * @copyright   Lp digital system
+ * @author      k.golovin
+ */
+class YmlLoaderTest extends TestCase
+{
+
+    protected $bbapp;
+    
+    protected function setUp()
+    {
+        $this->bbapp = $this->getBBApp();
+        $this->initDb($this->bbapp);
+        $this->initAcl();
+        
+        $superAdminGroup = new Group();
+        $superAdminGroup
+            ->setIdentifier('super_admin')
+            ->setName('Super Admin')
+        ;
+        $this->bbapp->getEntityManager()->persist($superAdminGroup);
+        
+        $adminGroup = new Group();
+        $adminGroup
+            ->setIdentifier('admin_front')
+            ->setName('Super Admin')
+        ;
+        $this->bbapp->getEntityManager()->persist($adminGroup);
+        
+        $this->bbapp->getEntityManager()->flush();
+    }
+    
+    public function testLoad_superadmin()
+    {
+        $site = new Site();
+        $site->setLabel('default');
+        $this->bbapp->getEntityManager()->persist($site);
+        $this->bbapp->getEntityManager()->flush();
+        
+        
+        $loader = new YmlLoader();
+        $loader->setContainer($this->bbapp->getContainer());
+        $loader->load(__DIR__ . '/acl.yml');
+        
+        $this->authUser('super_admin');
+        
+        $this->assertTrue($this->getSecurityContext()->isGranted('view', $site));
+        $this->assertTrue($this->getSecurityContext()->isGranted('EDIT', $site));
+    }
+    
+    public function testLoad_admin()
+    {
+        $site = new Site();
+        $site->setLabel('default');
+        $this->bbapp->getEntityManager()->persist($site);
+        $this->bbapp->getEntityManager()->flush();
+        
+        
+        $loader = new YmlLoader();
+        $loader->setContainer($this->bbapp->getContainer());
+        $loader->load(__DIR__ . '/acl.yml');
+        
+        $this->authUser('admin_front');
+        
+        $this->assertTrue($this->getSecurityContext()->isGranted('view', $site));
+        $this->assertFalse($this->getSecurityContext()->isGranted('EDIT', $site));
+    }
+    
+    /**
+     * 
+     * @param string $groupId
+     * @return \BackBuilder\Security\Token\BBUserToken
+     */
+    protected function authUser($groupId)
+    {
+        $token = new BBUserToken();
+        $user = new User();
+        $user
+            ->setLogin(uniqid('login'))
+            ->setPassword('pass')
+        ;
+        
+        $group = $this->bbapp->getEntityManager()->getRepository('BackBuilder\Security\Group')->findOneBy(array('_identifier' => $groupId));
+
+        if(!$group) {
+            throw new \RuntimeException('Group not found: ' . $groupId);
+        }
+        
+        $user->addGroup($group);
+        
+        $token->setUser($user);
+        $token->setAuthenticated(true);
+        
+        $security = $this->getSecurityContext();
+        /* @var $security \BackBuilder\Security\SecurityContext */
+
+        $security->setToken($token);
+
+        return $token;
+    }
+    
+    /**
+     * 
+     * @return \BackBuilder\Security\SecurityContext
+     */
+    private function getSecurityContext()
+    {
+        return $this->bbapp->getSecurityContext();
+    }
+
+}
