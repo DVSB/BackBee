5cafaf457594b30c15214236526257380ffa109f
Florian Kroockmann <florian.kroockmann@lp-digital.fr>
2014-08-25 16:08:12 +0200

    [FrontController] + add environement to nestednode command in terminate function

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require(__DIR__ . "/vendor/autoload.php");
+require(dirname(__DIR__) . "/vendor/autoload.php");
 require_once(__DIR__ . '/Tests/TestCase.php');
 
 $back_builder_unit_test = new \BackBuilder\Tests\TestCase();
