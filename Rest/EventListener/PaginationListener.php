74da5b40f0fd0c63ec66bfacfaf36bd9502219ba
Eric Chau <eric.chau@lp-digital.fr>
2014-09-05 17:37:39 +0200

    [Rest] + updated EventListener\PaginationListener to not proccess if it's not a rest request

--- Rest/EventListener/PaginationListener.php
+++ Rest/EventListener/PaginationListener.php
@@ -76,9 +76,12 @@ class PaginationListener extends APathEnabledListener
      */
     public function onKernelController(FilterControllerEvent $event)
     {
-        $controller = $event->getController();
-        $request = $event->getRequest();
+        $request = $this->request = $event->getRequest();
+        if (false === $this->isEnabled()) {
+            return;
+        }
 
+        $controller = $event->getController();
 
         if (false === in_array($request->getMethod(), array('GET', 'HEAD', 'DELETE')))  {
             // pagination only makes sense with GET or DELETE methods
