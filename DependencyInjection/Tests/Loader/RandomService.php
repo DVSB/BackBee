83399bbb574ea4ac7bd20e597e19fd20c434e548
Eric Chau <eric.chau@lp-digital.fr>
2014-09-08 13:19:04 +0200

    [DependencyInjection] + updated tests for Loader\ContainerProxy

--- DependencyInjection/Tests/Loader/RandomService.php
+++ DependencyInjection/Tests/Loader/RandomService.php
@@ -33,7 +33,7 @@ use BackBuilder\DependencyInjection\Dumper\DumpableServiceInterface;
 class RandomService implements DumpableServiceInterface
 {
     const DEFAULT_SIZE = 100;
-    const RANDOM_SERVICE_PROXY_CLASSNAME = '\BackBuilder\DependencyInjection\Tests\Loader\RandomServiceProxy';
+    const RANDOM_SERVICE_PROXY_CLASSNAME = 'BackBuilder\DependencyInjection\Tests\Loader\RandomServiceProxy';
 
     /**
      * @var int
