b53fd50f16fafe45a602872153452c8e6adccdc0
Eric Chau <eric.chau@lp-digital.fr>
2014-09-15 11:42:41 +0200

    [DependencyInjection] + updated Container::get() method and the way we manage dispatch of service tag event: every service has to implements DispatchTagEventInterface

---
+++ DependencyInjection/DispatchTagEventInterface.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * Copyright (c) 2011-2013 Lp digital system
+ *
+ * This file is part of BackBuilder5.
+ *
+ * BackBuilder5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BackBuilder5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace BackBuilder\DependencyInjection;
+
+/**
+ * This interface allow BackBuilder\DependencyInjection\Container to know if it needs to dispatch
+ * tags events or not
+ *
+ * @category    BackBuilder
+ * @package     BackBuilder\DependencyInjection
+ * @copyright   Lp digital system
+ * @author      e.chau <eric.chau@lp-digital.fr>
+ */
+interface DispatchTagEventInterface
+{
+    /**
+     * Define if current service need to dispatch tag event or not
+     *
+     * @return boolean true if needed to dispatch tag event, else false
+     */
+    public function needDispatchEvent();
+}
