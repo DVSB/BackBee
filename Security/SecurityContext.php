4b96eb3d7e21935701049c34c09a1eca5eec9757
Charles Rouillon <charles.rouillon@lp-digital.fr>
2014-09-22 15:00:03 +0200

    [SecurityContext] Fixng conflicts

--- Security/SecurityContext.php
+++ Security/SecurityContext.php
@@ -232,7 +232,7 @@ class SecurityContext extends sfSecurityContext
      */
     public function getACLProvider()
     {
-        return $this->aclprovider;
+        return $this->_aclprovider;
     }
 
     public function createProviders($config)
@@ -388,7 +388,7 @@ class SecurityContext extends sfSecurityContext
 
     private function createACLProvider($config)
     {
-        if (true === array_key_exists('acl', $config)) {
+        if (true === array_key_exists('acl', $config) && null !== $this->getApplication()->getEntityManager()) {
             if (true === isset($config['acl']['connection']) && 'default' === $config['acl']['connection']) {
                 if (false === $this->application->getContainer()->has('security.acl_provider')) {
                     $this->aclprovider = new \Symfony\Component\Security\Acl\Dbal\MutableAclProvider(
