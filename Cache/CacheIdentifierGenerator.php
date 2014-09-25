a4228286203ab9f62c3467c1211f6ff0f68a49af
Eric Chau <eric.chau@lp-digital.fr>
2014-09-25 18:43:52 +0200

    [Cache] + added new feature: CacheIdentifierGenerator which allows us to declare identifier appender to customize cache identifier

---
+++ Cache/CacheIdentifierGenerator.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * Copyright (c) 2011-2013 Lp digital system
+ *
+ * This file is part of BackBuilder5.
+ *
+ * BackBuilder5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BackBuilder5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace BackBuilder\Cache;
+
+use BackBuilder\Cache\IdentifierAppender\IdentifierAppenderInterface;
+use BackBuilder\DependencyInjection\ContainerInterface;
+use BackBuilder\Exception\InvalidArgumentException;
+use BackBuilder\Renderer\IRenderer;
+
+/**
+ * CacheIdentifierGenerator allows you to easily customize cache identifier by adding appenders
+ *
+ * @category    BackBuilder
+ * @package     BackBuilder\Cache
+ * @copyright   Lp digital system
+ * @author      e.chau <eric.chau@lp-digital.fr>
+ */
+class CacheIdentifierGenerator
+{
+    const APPENDER_SERVICE_TAG = 'cache.identifier.appender';
+
+    /**
+     * contains every declared appenders
+     *
+     * @var array
+     */
+    private $appenders;
+
+    /**
+     * constructor
+     *
+     * @param ContainerInterface $container service container from where we will retrieve every identifier appenders
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->appenders = array();
+        foreach (array_keys($container->findTaggedServiceIds(self::APPENDER_SERVICE_TAG)) as $appender_id) {
+            $this->addAppender($container->get($appender_id));
+        }
+    }
+
+    /**
+     * Add appender to current CacheIdentifierGenerator, sorted it by groups names
+     *
+     * @param IdentifierAppenderInterface $appender the appender to add
+     */
+    public function addAppender(IdentifierAppenderInterface $appender)
+    {
+        foreach ((array) $appender->getGroups() as $group_name) {
+            if (false === array_key_exists($group_name, $this->appenders)) {
+                $this->appenders[$group_name] = array();
+            }
+
+            $this->appenders[$group_name][] = $appender;
+        }
+    }
+
+    /**
+     * This method will compute cache identifier with every appenders that belong to group name
+     *
+     * @param  string    $group_name the group name of appenders to apply
+     * @param  string    $identifier identifier we want to update
+     * @param  IRenderer $renderer   the current renderer, can be null
+     *
+     * @return string the identifier new computed with appenders of group name
+     */
+    public function compute($group_name, $identifier, IRenderer $renderer = null)
+    {
+        if (false === $this->isValidGroup($group_name)) {
+            throw new InvalidArgumentException("$group_name is not a valid cache identifier appender group.");
+        }
+
+        foreach ($this->appenders[$group_name] as $appender) {
+            $identifier = $appender->computeIdentifier($identifier, $renderer);
+        }
+
+        return $identifier;
+    }
+
+    /**
+     * Define if provided group name is associated to any appenders or not
+     *
+     * @return boolean true if the provided group name is associated to one appender atleast, else false
+     */
+    public function isValidGroup($group_name)
+    {
+        return array_key_exists($group_name, $this->appenders);
+    }
+}
---
+++ Cache/IdentifierAppender/IdentifierAppenderInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * Copyright (c) 2011-2013 Lp digital system
+ *
+ * This file is part of BackBuilder5.
+ *
+ * BackBuilder5 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BackBuilder5 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace BackBuilder\Cache\IdentifierAppender;
+
+use BackBuilder\Renderer\IRenderer;
+
+/**
+ * Every cache identifier appender must implements this interface to be usable
+ *
+ * @category    BackBuilder
+ * @package     BackBuilder\Cache
+ * @copyright   Lp digital system
+ * @author      e.chau <eric.chau@lp-digital.fr>
+ */
+interface IdentifierAppenderInterface
+{
+    /**
+     * This method allows every identifier appender to customize cache identifier with its own logic
+     *
+     * @param  string    $identifier the identifier to update if needed
+     * @param  IRenderer $renderer   the current renderer, can be null
+     *
+     * @return string return the new identifier
+     */
+    public function computeIdentifier($identifier, IRenderer $renderer = null);
+
+
+    /**
+     * Returns every group name this appender is associated with
+     *
+     * @return array
+     */
+    public function getGroups();
+}
