9145997c82b77e5042f5cb5a552c897973cd07f7
Eric Chau <eric.chau@lp-digital.fr>
2014-10-29 10:48:06 +0100

    [Rest] #2800: + updated Patcher\RightManager to work with JMS\Serializer ClassMetadata instead of an yaml config file

--- Rest/Patcher/RightManager.php
+++ Rest/Patcher/RightManager.php
@@ -20,9 +20,11 @@ namespace BackBuilder\Rest\Patcher;
  * along with BackBuilder5. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use BackBuilder\Rest\Patcher\PatcherInterface;
+use Metadata\MetadataFactoryInterface;
 
 /**
+ * RightManager is able to build a mapping of authorized action on entity's properties with
+ * the provided Metadata\MetadataFactoryInterface
  *
  * @category    BackBuilder
  * @package     BackBuilder\Rest
@@ -32,37 +34,45 @@ use BackBuilder\Rest\Patcher\PatcherInterface;
 class RightManager
 {
     /**
-     * [$rights description]
+     * This factory will be used to build authorization mapping
      *
-     * @var [type]
+     * @var \Metadata\MetadataFactoryInterface
+     */
+    private $metadata_factory;
+
+    /**
+     * mapping of entity namespace and properties authorized actions
+     *
+     * @var array
      */
     private $rights;
 
     /**
-     * [__construct description]
+     * RightManager's constructor
      *
-     * @param array $rights [description]
+     * @param MetadataFactoryInterface $metadata_factory the factory to use to build authorization mapping
      */
-    public function __construct(array $rights)
+    public function __construct(MetadataFactoryInterface $metadata_factory)
     {
-        $this->buildRights($rights);
+        $this->metadata_factory = $metadata_factory;
+        $this->rights = array();
     }
 
     /**
-     * [authorized description]
+     * Return true if the $operation is authorized on $entity's $attribute, else false
      *
-     * @param  object $entity    [description]
-     * @param  [type] $attribute [description]
-     * @param  [type] $operation [description]
+     * @param  object $entity
+     * @param  string $attribute
+     * @param  string $operation
      *
-     * @return [type]            [description]
+     * @return boolean true if $operation is authorized, else false
      */
     public function authorized($entity, $attribute, $operation)
     {
         $authorized = true;
         $classname = get_class($entity);
         if (false === array_key_exists($classname, $this->rights)) {
-            $authorized = false;
+            $this->buildRights($classname);
         }
 
         $attribute = str_replace('/', '', $attribute);
@@ -78,17 +88,59 @@ class RightManager
     }
 
     /**
-     * [buildRights description]
+     * Builds the authtorization mapping for the given $classname
      *
-     * @param  array  $rights [description]
+     * @param  string $classname
      */
-    private function buildRights(array $rights)
+    private function buildRights($classname)
     {
-        foreach ($rights as $classname => $permissions) {
-            $this->rights[$classname] = array();
-            foreach ($permissions['attributes'] as $attr) {
-                $this->rights[$classname][$attr] = (array) $permissions['valid_operations'];
+        $metadatas = $this->metadata_factory->getMetadataForClass($classname);
+        $reflection = new \ReflectionClass($classname);
+
+        $this->rights[$classname] = array();
+        if (null !== $metadatas) {
+            foreach ($metadatas->propertyMetadata as $property_name => $property_metadata) {
+                $property_name = $this->cleanPropertyName($property_name);
+                $this->rights[$classname][$property_name] = array();
+
+                if (
+                    false === $property_metadata->readOnly
+                    && $reflection->hasMethod($this->buildProperMethodName('set', $property_name))
+                ) {
+                    $this->rights[$classname][$property_name][] = 'replace';
+                }
             }
         }
+    }
+
+    /**
+     * This method will replace '_' by '' of $property_name if its first letter is an underscore (_)
+     *
+     * @param  string $property_name the property we want to clean
+     *
+     * @return string cleaned property name
+     */
+    private function cleanPropertyName($property_name)
+    {
+        return preg_replace('#^_([\w_]+)#', '$1', $property_name);
+    }
+
+    /**
+     * Builds a valid method name for property name; Replaces every '_' by ''
+     * and apply ucfirst to every words seperated by an underscore
+     *
+     * @param  string $prefix        the prefix to prepend to the method name (example: 'get', 'set', 'is')
+     * @param  string $property_name
+     *
+     * @return string a valid method name
+     */
+    private function buildProperMethodName($prefix, $property_name)
+    {
+        $method_name = explode('_', $property_name);
+        $method_name = array_map(function ($str) {
+            return ucfirst($str);
+        }, $method_name);
+
+        return $prefix . implode('', $method_name);
     }
 }
