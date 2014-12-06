36105a03340a1218ad517f2ed79e9888707d68cc
Eric Chau <eriic.chau@gmail.com>
2014-12-06 13:24:13 +0000

    + added newline character to every text files

--- Resources/Installer.php
+++ Resources/Installer.php
@@ -3,8 +3,10 @@ namespace BackBuilder\Resources;
 
 use Composer\Script\Event;
 
-class Installer {
-    public static function downloadBBCoreJS(Event $event) {
+class Installer
+{
+    public static function downloadBBCoreJS(Event $event)
+    {
         exec('git clone https://github.com/Lp-digital/BbCoreJs.git ' . __DIR__ . DIRECTORY_SEPARATOR . 'toolbar');
         putenv('BOWERPHP_TOKEN=ce486984b0412db0fffeabf526a0a22af364c07d');
     }
