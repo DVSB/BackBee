1ad8b182e63c0d7426423229a7b9010622fcd727
Charles Rouillon <charles.rouillon@lp-digital.fr>
2015-01-05 14:01:29 +0100

    [Tests] Add TestCase::invokeMethod(&$object, $methodName, array $parameters = array()) in order to test private functions

--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -262,4 +262,24 @@ class TestCase extends \PHPUnit_Framework_TestCase
         $schema = new SchemaTool($em);
         $schema->dropSchema($metadata);
     }
+
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object &$object    Instantiated object that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     * @link https://jtreminio.com/2013/03/unit-testing-tutorial-part-3-testing-protected-private-methods-coverage-reports-and-crap/
+     */
+    public function invokeMethod(&$object, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
+
 }
