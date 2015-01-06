b269bfa8b932007792066aa44f31812d16973716
Charles Rouillon <charles.rouillon@lp-digital.fr>
2015-01-06 19:28:02 +0100

    [NestedNode] #11 - Remove not nullable constraint on Page::_section because of chained flushes of Doctrine
    Conflicts:
    	NestedNode/Page.php

--- NestedNode/Page.php
+++ NestedNode/Page.php
@@ -288,7 +288,7 @@ class Page extends AObjectIdentifiable implements IRenderable, DomainObjectInter
      * The section node.
      * @var \BackBuilder\NestedNode\Section
      * @ManyToOne(targetEntity="BackBuilder\NestedNode\Section", inversedBy="_pages", cascade={"persist"}, fetch="EXTRA_LAZY")
-     * @JoinColumn(name="section_uid", referencedColumnName="uid", nullable=false)
+     * @JoinColumn(name="section_uid", referencedColumnName="uid")
      */
     public $_section;
 
