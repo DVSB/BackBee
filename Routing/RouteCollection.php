f91a86dd4fd1564e543eee26117f699a2c05970c
Eric Chau <eric.chau@lp-digital.fr>
2015-04-22 15:41:21 +0200

    [Routing] fixed RouteCollection::getUri to return valid url (replacing '\' by '/')

--- Routing/RouteCollection.php
+++ Routing/RouteCollection.php
@@ -229,7 +229,7 @@ class RouteCollection extends sfRouteCollection implements DumpableServiceInterf
             $pathinfo .= $defaultExt;
         }
 
-        return $pathinfo;
+        return str_replace('\\', '/', $pathinfo);
     }
 
     /**
